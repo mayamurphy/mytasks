cd391130b6cb61f21ebb66c463755cc2d2ddb178
Maya Murphy <mayamurphy@u.boisestate.edu>
2024-02-22 18:54:09 -0700

    fix image name

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
       <link id="header-pfp" rel="icon" type="image/png" href="images/mytasks.png"/>
       <label for="account">username </label>
         <div id="user-pfp-and-menu">
-          <img src="images/default_pfp.png">
+          <img src="images/Default_pfp.png">
           <select name="menu-options" id="menu-options">
             <option></option>
             <option><a href="Settings.php">Account</option>
